40511b603b9f7a9779be6f5ab2b06109e347dcd0
Sören Krokfors <soren.krokfors@leanders.fi>
2020-03-21 09:25:32 +0200

    Feat(test): added small sample

--- src/PCOMLibrary.Tests/Examples.cs
+++ src/PCOMLibrary.Tests/Examples.cs
@@ -1,14 +1,45 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unitronics.PCOM;
+using Unitronics.PCOM.ReadOperations;
+using Unitronics.PCOM.WriteOperations;
+using Unitronics.Utils;
 
-namespace PCOM.Library.Tests
+namespace PCOMLibrary.Tests
 {
     [TestClass]
     public class Examples
     {
         [TestMethod]
-        public void CreateAndSendReciveMessage()
+        public async Task CreateAndSendReciveMessage()
         {
-
+            var  cMessage=new CommunicationMessage();
+            var MI10 = new MIReadOperation(10, 1);
+            cMessage.AddOperation(MI10);
+            cMessage.AddOperation(new MIWriteOperation(12,1));
+            var client=new PcomTcpClient(new CancellationTokenSource(),new FileLogger(new FileLoggerSettings
+            {
+                BytesPerLine = 16,
+                FileName = string.Empty, //Use this file to hexdump messages sent
+                ShowAscii = false,
+                ShowHeader = true,
+                ShowOffset = true
+            }));
+            
+            await client.TcpClient.ConnectAsync("192.168.100.101", 20258);
+            if (client.TcpClient.Connected)
+            {
+                var response = await client.SendAndReceive(cMessage.GetMessage());
+                cMessage.ParseMessage(response);
+                Assert.AreNotEqual(0, MI10);
+            }
+            else
+            {
+                throw  new Exception("Not Connected");
+            }
         }
     }
 }
