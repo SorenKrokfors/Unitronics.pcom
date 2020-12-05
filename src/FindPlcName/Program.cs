7247a325e610ce4083fe138a73f60981cd163814
Sören Krokfors <soren.krokfors@leanders.fi>
2020-12-05 14:58:05 +0200

    Added FindPlcName

---
+++ src/FindPlcName/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Unitronics.PCOM;
+
+namespace FindPlcName
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("PlcName IP Port");
+                Console.WriteLine("Example: PlcName 192.168.100.10 20256");
+            }
+            else
+            {
+                try
+                {
+                    var plc = new PcomTcpClient(new CancellationTokenSource(), null);
+                    await plc.TcpClient.ConnectAsync(args[1], int.Parse(args[2]));
+                    var message = new CommunicationMessage();
+                    var plcName = plc.SendAndReceive(message.GetPlcName());
+                    Console.WriteLine("PlcName: " + plcName);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Något gick fel!, Kontrollera IP och Port");
+                    Console.WriteLine("Example: PlcName 192.168.100.10 20256");
+                }
+                     
+            }
+
+        }
+    }
+}
---
+++ src/PCOMLibrary/PCOM/PCOMRS232Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unitronics.PCOM
+{
+    public class PCOMRS232Client
+    {
+    }
+}
